46a6aaad7b2646c274cfb5ef0d21bd27c94252cb
venkata1pavan <146666225+venkata1pavan@users.noreply.github.com>
2023-12-03 02:20:29 +0300

    Add files via upload

---
+++ mycontacts/contactsapi/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+
+    
---
+++ mycontacts/contactsapi/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ContactSerializer
+from .models import Contact
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all().order_by('first_name')
+    serializer_class = ContactSerializer
